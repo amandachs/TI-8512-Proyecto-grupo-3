a0a7dd9bb369b55b5b19d7a22f905dc8c1148786
Sebastián Navarro <sj.navarro026@gmail.com>
2025-05-24 23:18:56 -0600

    feat: SearchBar component and background image

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import RecetasGrid from './components/RecetasGrid';
-import CategoriasGrid from './components/CategoriasGrid';
+// src/App.tsx
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import SearchBar from "./components/SearchBar";
 
-const App: React.FC = () => {
+const RecetaDetalle = () => (
+  <div style={{ padding: '2rem', fontSize: '1.5rem', textAlign: 'center' }}>
+    <p>Detalle de la receta (ruta simulada)</p>
+  </div>
+);
+
+function App() {
   return (
     <Routes>
-      <Route path="/" element={<CategoriasGrid />} />
-      <Route path="/categoria/:nombre" element={<RecetasGrid />} />
+      <Route path="/" element={<SearchBar />} />
+      <Route path="/detalle/:id" element={<RecetaDetalle />} />
     </Routes>
   );
-};
+}
 
 export default App;
